8bd18d17bef221638d8588401a90746b7d2e0e8b
Mihail3224 <bagrinmihai5@gmail.com>
2024-02-13 10:14:35 +0200

    Adaugare CRUD

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
 
 class CarController extends Controller
 {
-    //
+    public function index()
+    {
+        // Afișarea tuturor mașinilor
+    }
+
+    public function create()
+    {
+        // Afișarea formularului pentru crearea unei noi mașini
+    }
+
+    public function store(Request $request)
+    {
+        // Salvarea unei noi mașini în baza de date
+    }
+
+    public function show(Car $car)
+    {
+        // Afișarea detaliilor despre o mașină specifică
+    }
+
+    public function edit(Car $car)
+    {
+        // Afișarea formularului pentru editarea unei mașini
+    }
+
+    public function update(Request $request, Car $car)
+    {
+        // Actualizarea detaliilor unei mașini
+    }
+
+    public function destroy(Car $car)
+    {
+        // Ștergerea unei mașini din baza de date
+    }
 }
--- app/Http/Controllers/MechanicController.php
+++ app/Http/Controllers/MechanicController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mechanic;
 
 class MechanicController extends Controller
 {
-    //
+    public function index()
+    {
+        // Afișarea tuturor mecanicilor
+    }
+
+    public function create()
+    {
+        // Afișarea formularului pentru crearea unui nou mecanic
+    }
+
+    public function store(Request $request)
+    {
+        // Salvarea unui nou mecanic în baza de date
+    }
+
+    public function show(Mechanic $mechanic)
+    {
+        // Afișarea detaliilor despre un mecanic specific
+    }
+
+    public function edit(Mechanic $mechanic)
+    {
+        // Afișarea formularului pentru editarea unui mecanic
+    }
+
+    public function update(Request $request, Mechanic $mechanic)
+    {
+        // Actualizarea detaliilor unui mecanic
+    }
+
+    public function destroy(Mechanic $mechanic)
+    {
+        // Ștergerea unui mecanic din baza de date
+    }
 }
